ad7578075dcddddee487bd8c79e59bef1064f634
talpro6370 <45435657+talpro6370@users.noreply.github.com>
2020-03-13 17:46:13 +0200

    Add files via upload

---
+++ Interfaces/IAnonymousUserFacade.cs
@@ -0,0 +1,25 @@
+﻿using MainProject.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject.InterFaces
+{
+    interface IAnonymousUserFacade
+    {
+        List<Flight> GetAllFlights();
+        List<AirlineCompany> GetAllAirlineCompanies();
+        Dictionary<Flight, int> GetAllFlightsVacancy();
+        Flight GetFlightById(int id);
+        List<Flight> GetFlightsByOriginCountry(int countryCode);
+        List<Flight> GetFlightsByDestinationCountry(int countryCode);
+        List<Flight> GetFlightsByDepatrureDate(DateTime departureDate);
+        List<Flight> GetFlightsByLandingDate(DateTime landingDate);
+        Tickets GetTicketByFlightId(int id);
+        List<DeparturesPoco> GetDepartureDetails();
+        List<ArrivalsPoco> GetArrivalsDetails();
+        List<FlightInfoFromDb> GetFlightsByDesCountryNameFromDb(string countryName);
+    }
+}
---
+++ Interfaces/IHelperDAO.cs
@@ -0,0 +1,20 @@
+﻿using MainProject.DAO;
+using MainProject.Pocos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProject.InterFaces
+{
+    public interface IHelperDAO
+    {
+        List<DeparturesPoco> DeparturesDetails();
+        List<ArrivalsPoco> ArrivalsDetails();
+        List<FlightInfoFromDb> GetFlightsByDesCountryNameFromDb(string countryName);
+        List<CustomerInfoByAirlineName> GetFlightsByAirlineName(string airlineName);
+        List<CustomerInfoByAirlineName> GetFlightsByOriginCountryName(string originCountryName);
+        List<CustomerInfoByAirlineName> GetFlightsByFlightNum(long flightNum);
+    }
+}
